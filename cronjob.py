d79669881829c7d38274244dd541fd500aede74b
Vladimir ROmanichev <pro100print@yahoo.com>
2020-07-16 21:46:25 +0300

    test at time

--- cronjob.py
+++ cronjob.py
@@ -5,7 +5,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from main import cronjob
 
 # Create an instance of scheduler and add function.
-scheduler = BlockingScheduler()
-scheduler.add_job(cronjob, "interval", hours=1)
+scheduler = BlockingScheduler(timezone='Europe/Kiev')
+scheduler.add_job(cronjob, 'cron', day_of_week='mon-fri', hour=21, minute=50)
 
 scheduler.start()
